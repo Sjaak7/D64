c8a3bfe649c96465b6c3ff52f8cb03139ea5f58d
Sjaak7 <renssparrius@gmail.com>
2020-01-15 17:11:21 +0100

    first index.php

---
+++ webroot/index.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+ini_set('display_errors', "1");
+ini_set('display_startup_errors', "1");
+error_reporting(E_ALL);
+
+define('URL','https://d64.nl');
+
+include('./includes/d64.php');
+
+$d64 = new d64();
+$d64->header = new d64Header($d64);
+$d64->footer = new d64Footer($d64);
+
+$d64->header->set_description("Test website van Gerda");
+
+// Check if we are on the frontpage without any jokes like querystrings.. holla hop
+if(!isset($d64->path[0]) && empty($_SERVER['QUERY_STRING'])){
+	$d64->set_content(
+		'<h2>Welkom</h2>'.
+		'<p>Dit is een test project van Gerda. Gerda (m) is een eenzame sysop.. je kunt hem gerust een berichtje sturen maar houd het wel netjes ajb. P.S. ik ben niet zo goed met kleuren.</p>'.
+		'<div id="cB"></div>'.
+		'<div class="w3-small w3-margin-top" id="cN"></div>'.
+		'<div class="w3-bar w3-margin-top">'.
+			'<input class="w3-input w3-border w3-bar-item" id="c" type="text" maxlength="9">'.
+		'</div>'
+	);
+}elseif($d64->path[0]==='offline'){
+	$d64->set_content(
+		'<h2>Geen verbinding</h2>'.
+		'<p>Je hebt geen verbinding met internet. Ik probeer het opnieuw als de verbinding hersteld is.</p>'
+	);
+}
+
+$d64->init();
+
+$bitpay = json_decode(file_get_contents(ROOTPATH.'/cache/btc.json'),true);
+if(is_array($bitpay)){
+  $bitprice = $bitpay[3]['rate'];
+}else $bitprice = 'NaN';
+
+$d64->header->set_script('<script src="/js/d64.js"></script>');
+
+echo $d64->header->make_header().
+     '<div class="w3-row">'.
+       '<div class="w3-quarter w3-green w3-padding">'.
+		'<div>'.
+			'<h3>Laatste</h3>'.
+			$d64->module('blog')->get_news().
+		'</div>'.
+		'<div>'.
+			'<h3>Crypto</h3>'.
+			'Bitcoin: &euro; <span id="btc_euro">'.$bitprice.'</span>'.
+		'</div>'.
+		'<h3>Live</h3>'.
+		// ytp = youtube placeholder
+		'<div id="ytp">'.
+			// ytvi youtube image..
+			'<img src="/img/live.png" class="w3-image" alt="Youtube live, play knop" id="ytvi">'.
+		'</div>'.
+       '</div>'.
+       '<div class="w3-threequarter w3-dark-grey w3-padding">'.
+         $d64->get_content().
+       '</div>'.
+     '</div>'.
+     $d64->footer->make_footer();
+
+?>

935888b648ae2bddb0004ea64cac60510a1ab6d3
Sjaak7 <renssparrius@gmail.com>
2020-01-16 06:34:26 +0100

    Changed git URL

--- webroot/includes/d64footer.php
+++ webroot/includes/d64footer.php
@@ -13,7 +13,7 @@ class d64Footer{
   {
     return '<div class="w3-container w3-teal">'.
            '<p class="w3-center w3-small">'.
-             '<a href="/blog/">Blog</a> - <a href="https://github.com/Sjaak7/d64">Code</a> - &copy; 2020 - <a href="https://d64.nl">D64.nl</a>'.
+             '<a href="/blog/">Blog</a> - <a href="https://github.com/Sjaak7/D64">Code</a> - &copy; 2020 - <a href="https://d64.nl">D64.nl</a>'.
            '</p>'.
            '</div>'.
            '</body>'.
